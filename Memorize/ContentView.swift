93d7adb35485c8e5fa0af7d0cebffdc4c2b31e51
Rhett L <webster117@hotmail.com>
2022-06-08 10:15:02 -0600

    initial commit, added View, Model, ViewModel.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,14 +8,87 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var viewModel: EmojiMemoryGame
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 70))] ) {
+                ForEach(viewModel.cards) { card in
+                    CardView(card: card)
+                        .aspectRatio(2/3, contentMode: .fit)
+                        .onTapGesture {
+                            viewModel.choose(card)
+                        }
+                }
+            }
+        }
+        .foregroundColor(.red)
+        .padding(.horizontal)
+        
+    }
+//    var vehicleTheme: some View {
+//        Button {
+//            emojis = ["🚗", "🚙", "🏎", "🛻", "🚜", "🚌", "🚐", "🚛"]
+//            emojiCount = emojis.count
+//            emojis.shuffle()
+//        } label: {
+//            Image(systemName: "car")
+//        }
+//    }
+//    var facesTheme: some View {
+//        Button {
+//            emojis = ["🙂", "😄", "😎", "😋", "👽", "🥰", "🥸", "😇", "🥳", "😱", "🤠", "💩", "🥴", "😵", "🫥", "😑"]
+//            emojiCount = emojis.count
+//            emojis.shuffle()
+//        } label: {
+//            Image(systemName: "face.smiling")
+//        }
+//    }
+//    var animalTheme: some View {
+//        Button {
+//            emojis = ["🦎", "🐸", "🐍", "🦜", "🦄", "🦖", "🦕", "🐊", "🦤", "🦆", "🐧", "🐣", "🦨", "🐢", "🐙", "🦑", "🦭", "🦦", "🦥", "🦔", "🐷", "🐉", "🐓", "🪱"]
+//            emojiCount = emojis.count
+//            emojis.shuffle()
+//        } label: {
+//            Image(systemName: "pawprint")
+//        }
+//    }
+
+}
+
+struct CardView: View {
+    let card: MemoryGame<String>.Card
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 15.0)
+            if card.isFaceUp {
+                shape.fill(.white)
+                shape.strokeBorder(lineWidth: 4)
+                Text(card.content).font(.largeTitle)
+            } else {
+                shape.fill(.red)
+            }
+        }
     }
 }
 
+
+
+
+
+
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        let game = EmojiMemoryGame()
+        ContentView(viewModel: game)
+            .preferredColorScheme(.dark)
+        ContentView(viewModel: game)
     }
 }
+
+
---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,32 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Rhett Levitz on 6/7/22.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame: ObservableObject {
+    
+     static let emojis = ["🚗", "🚙", "🏎", "🛻", "🚜", "🚌", "🚐", "🚛"]
+
+    static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
+            emojis[pairIndex]
+        }
+    }
+    
+    @Published private var model: MemoryGame<String> = createMemoryGame()
+    
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    // MARK: - Intents
+    
+    func choose(_ card: MemoryGame<String>.Card) {
+        model.choose(card)
+    }
+}
