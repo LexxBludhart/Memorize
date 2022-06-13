b9c739a66372b09744010bf0b000bbf196966052
Rhett L <webster117@hotmail.com>
2022-06-13 12:46:22 -0600

    added Theme struct with some inits

---
+++ Memorize/Theme.swift
@@ -0,0 +1,16 @@
+//
+//  Theme.swift
+//  Memorize
+//
+//  Created by Rhett Levitz on 6/13/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct Theme {
+    
+    var emojis = [String]()
+    var cardColor = Color(red: <#T##Double#>, green: <#T##Double#>, blue: <#T##Double#>)
+    var numberOfPairs: Int
+}
